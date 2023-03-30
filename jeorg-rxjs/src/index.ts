8d5256be537d3fd5778853b7ace82320bf7c0b43
Joao Esperancinha <jofisaes@gmail.com>
2023-03-30 16:37:05 +0200

    Runs first successful pipes with Pure RxJS

--- jeorg-rxjs/src/index.ts
+++ jeorg-rxjs/src/index.ts
@@ -1,18 +1,57 @@
-import { Observable } from 'rxjs';
-var observable = Observable.create((observer:any) => {
+import {flatMap, map, Observable, Observer} from 'rxjs';
+
+class Vegetable {
+    name: string;
+    size: number;
+}
+
+const observable = Observable.create((observer: any) => {
     observer.next('Hello World!');
     observer.next('Hello Again!');
     observer.complete();
     observer.next('Bye');
-})
+});
+
+const observable2 = Observable.create((observer: Observer<Vegetable[]>) => {
+    observer.next([{
+        "name": "tomato",
+        "size": 10
+    },
+        {
+            "name": "pineapple" ,
+            "size": 50
+        }])
+    observer.complete()
+    return observer
+}).pipe(
+    map(value => {
+        console.log(value)
+        return value
+    }),
+    flatMap(value => value as any)
+)
+
+observable2.subscribe(
+    (x: Vegetable) => pushName(x)
+)
 observable.subscribe(
-    (x:any) => logItem(x),
-    (error: any) => logItem ('Error: ' + error),
+    (x: any) => logItem(x),
+    (error: any) => logItem('Error: ' + error),
     () => logItem('Completed')
 );
-function logItem(val:any) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(val);
+
+function logItem(val: any) {
+    console.log(val)
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(val);
     node.appendChild(textnode);
     document.getElementById("list").appendChild(node);
+}
+
+function pushName(x: Vegetable) {
+    console.log(x)
+    const node = document.createElement("li");
+    const textnode = document.createTextNode(x.name);
+    node.appendChild(textnode);
+    document.getElementById("vegetables").appendChild(node);
 }
